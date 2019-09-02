7c06e491a9e451d1d8816451992f68a3a2d2bc13
ctmatt <54801045+ctmatt@users.noreply.github.com>
2019-09-02 21:04:41 +1000

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,3 +1,5 @@
+Start-Sleep -Seconds 3
+
 #Download updated script
 $dl = (New-Object System.Net.WebClient).Downloadstring('https://raw.githubusercontent.com/ctmatt/PapercutReporting/master/PapercutNotification.ps1?token=ANCDFFKEPFMBVLFICGAYXFS5NT3FS')
 
